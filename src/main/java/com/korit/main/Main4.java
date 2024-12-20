64c0a7550984310edfa76dfbae7f471624b220c0
bsoupb <qotn0327@naver.com>
2024-12-20 17:10:37 +0900

    2024-12-20 자바 익명, 람다식

---
+++ src/main/java/com/korit/main/Main4.java
@@ -0,0 +1,34 @@
+package com.korit.main;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class Main4 {
+    public static void main(String[] args) {
+        List<Integer> numberList = List.of(1, 2, 3, 4, 5, 6, 7, 8, 9, 10);
+        List<Integer> numberList2 = null;
+        List<Integer> numberList3 = null;
+
+        numberList2 = new ArrayList<>();
+
+        for(int i = 0; i < numberList.size(); i++){
+            Integer num = numberList.get(i);
+            numberList2.add(num * 10);
+        }
+        /*
+        for(Integer num : numberList) {
+            numberList2.add(num * 10);
+        }
+
+        numberList.forEach(numberList2::add);
+        */
+
+        numberList3 = numberList.stream().map(n -> n * 10).collect(Collectors.toList());
+
+
+        System.out.println(numberList);
+        System.out.println(numberList2);
+        System.out.println(numberList3);
+    }
+}
